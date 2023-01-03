b33a35ad6c77cef018d15d25988c06c73817be35
micahelrkytch <michaelr.kytch@gmail.com>
2023-01-02 22:37:07 -0800

    days 8 and 9

--- src/advent_of_clerk/day_07.clj
+++ src/advent_of_clerk/day_07.clj
@@ -1,3 +1,158 @@
 ;; # 🎄 Advent of Clerk: Day 7
 (ns advent-of-clerk.day-07
-  (:require [nextjournal.clerk :as clerk]))
+  (:require [nextjournal.clerk :as clerk]
+            [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.pprint :as pp]
+            [clojure.set :as set]))
+
+
+(def input (slurp (io/resource "aoc-input-day8.txt")))
+
+(defn parse-input [input]
+  (->> input
+      str/split-lines
+       (map vec)
+       (map (fn [coll] (vec (map #(Character/digit % 10) coll))))
+       vec))
+
+
+;; Generate 4 sequences of coordinates, representing scans of the matrix from all 4 directions
+;; Apply f to each scan sequence -- e.g. scanning rows left to right,
+;; call (f grid (range n)), where n is the number of entries in a row
+;; returns a list of 4 row results, one for each scan direction (lr rl tb bt)
+(defn visit-trees [f grid]
+  (let [nrows (count grid)
+        ncols (count (first grid))
+        lr (for [y (range nrows)
+                 :let [row-set (f grid (for [x (range ncols)]
+                                         [x y]))]]
+             row-set
+             )
+        rl (for [y (range nrows)
+                 :let [row-set (f grid (for [x (reverse (range ncols))]
+                                         [x y]))]]
+             row-set
+             )
+        tb (for [y (range nrows)
+                 :let [row-set (f grid (for [x (range ncols)]
+                                         [y x]))]]
+             row-set
+             )
+        bt (for [y (range nrows)
+                 :let [row-set (f grid (for [x (reverse (range ncols))]
+                                         [y x]))]]
+             row-set
+             )
+        ]
+    (list lr rl tb bt))
+  )
+
+;; Part 1
+
+;; Return the set coordinates for the tallest trees in the given sequence of coordinates
+;; grid: the game board
+;; coords: sequence of [x y] coordinates for a row
+(defn tallest-in-row [grid coords]
+
+  ;; Reducing function takes the state value [visible max]
+  ;; visible: set of coordinates of the visible trees
+  ;; max height of tallest tree seen so far in the row
+  ;; and the sequence value
+  ;; x: current index within the row
+  (let [rfn (fn [[visible max :as current-state]
+                 [x y :as pos]]
+
+              ;; grid is stored as a vector of row vectors, so we index into the
+              ;; row (y) and then the column (x)
+              (let [curr (get-in grid [y x])]
+                (if (> curr max)
+                  ;; if current tree is taller than max, mark it visible and update max
+                  [(conj visible pos) curr]
+                  ;; else, return state unchanged
+                  current-state
+                  )))
+        ;; Initial state -- we have found no tall trees yet, and max height is -1
+        initial-state [#{} -1]]
+    (first (reduce rfn initial-state coords))
+    ))
+
+
+(->> input
+    parse-input
+     ;; Get tallest trees for each row from each scan direction
+     (visit-trees tallest-in-row)
+     ;; Union all the sets of tallest tree coordinates
+     (reduce concat)
+     (reduce set/union)
+     ;; Count total
+     count
+     )
+
+;; Part 2
+
+;; Return a vector of visibility values for each entry
+;; where visibility is the number of continuous entries that is <= the current entry
+;; in the coordinate sequence so far
+;; grid: the game board
+;; coords: sequence of [x y] coordinates for a row
+(defn visibilities [grid coords]
+  ;; Reducing function takes the state value [scores last-height]
+  ;; scores: vector of visibility scores for the sequence so far
+  ;; last-height: height of the previous entry in the sequence
+  ;; and the sequence value
+  ;; x: current index within the row
+  (let [rfn (fn [[scores last-height :as current-state]
+                 [x y]]
+              (let [last-score (last scores)
+                    ;; grid is stored as a vector of row vectors, so we index into the
+                    ;; row (y) and then the column (x)
+                    curr-height (get-in grid [y x])
+                    curr-score
+                    (if last-score
+                      (if (> curr-height last-height)
+                        ;; if current tree is taller the previous entry, increment the score
+                        (inc last-score)
+                        ;; else the current tree is blocked, so it's score resets to 1
+                        1
+                        )
+                      ;; the first entry always has score 0
+                      0
+                      )]
+                [(conj scores curr-score) curr-height]))
+        ;; Initial state -- no scores yet, and last height is 0
+        initial-state [[] 0]]
+    (first (reduce rfn initial-state coords))))
+
+
+(defn reorder-matrices [[lr rl tb bt]]
+  (let [lr (vec lr)
+        ;; reverse right-to-left matrix rows
+        rl (mapv (comp vec reverse) rl)
+        ;; transpose top-to-bottom matrix so that it becomes left-to-right
+        tb (apply mapv vector tb)
+        ;; reverse then transpose
+        bt (->> bt
+                (mapv (comp vec reverse))
+                (apply mapv vector)
+                )]
+    [lr rl tb bt])
+  )
+(->> input
+     parse-input
+     ;; Get matrix of visibility scores for each scan direction
+     (visit-trees visibilities)
+     ;; Reorder matrices so that they're all in the same coordinate system
+     ;; TODO: this is not great.  Maybe it would have been better to put all the scores into
+     ;; a single matrix
+     reorder-matrices
+     ;; Concat the rows of each matrix into one vector
+     (map #(apply concat %))
+     ;; multiply 4 matrices in parallel to get the visibility score for each entry
+     (apply map *)
+     ;; get max
+     (apply max)
+     )
+
+;; TODO: part 2 is producing 192, which apparently is too small
+
--- src/advent_of_clerk/day_08.clj
+++ src/advent_of_clerk/day_08.clj
@@ -1,3 +1,125 @@
 ;; # 🎄 Advent of Clerk: Day 8
 (ns advent-of-clerk.day-08
-  (:require [nextjournal.clerk :as clerk]))
+  :require
+  [nextjournal.clerk :as clerk]
+  [clojure.string :as str]
+  [clojure.java.io :as io])
+
+(def input (slurp (io/resource "aoc-input-day9.txt")))
+
+(def sample "R 4\nU 4\nL 3\nD 1\nR 4\nD 1\nL 5\nR 2")
+
+(defn parse-line [line]
+  (let [[dir n] (str/split line #" ")]
+    [(first dir) (parse-long n)]))
+
+;; Process a line form the input
+;; Generates a sequence of [x y] deltas
+(defn cmd-to-moves [[dir n]]
+  (let [moveDelta {\U [1 0]
+                   \D [-1 0]
+                   \L [0 -1]
+                   \R [0 1]}]
+    (repeat n (moveDelta dir))
+    ))
+
+;; Map position difference to tail move
+(def tail-moves
+  {[0 2]   [0 -1]
+   [1 2]   [-1 -1]
+   [-1 2]  [1 -1]
+   [0 -2]  [0 1]
+   [-1 -2] [1 1]
+   [1 -2]  [-1 1]
+   [2 0]   [-1 0]
+   [2 1]   [-1 -1]
+   [2 2]   [-1 -1]
+   [2 -1]  [-1 1]
+   [2 -2] [-1 1]
+   [-2 0]  [1 0]
+   [-2 -1] [1 1]
+   [-2 -2] [1 1]
+   [-2 1]  [1 -1]
+   [-2 2] [1 -1]
+   })
+
+;; Given a new head position, where should tail move to
+;; Return new tail position
+(defn next-tail [head tail]
+  (let [delta (map - tail head)]
+    (if-let [move (tail-moves delta)]
+      (mapv + tail move)
+      ;; default: tail does not move
+      tail)))
+
+;; For a given move, calculate the next head and tail positions
+;; for two adjacent knots (relative head/tail)
+(defn next-segment-coords [[head tail] move]
+  (let [new-head (vec (map + head move))
+        new-tail (vec (next-tail new-head tail))]
+    [new-head new-tail]))
+
+;; Part 1
+
+(defn process-input1 [input]
+  (let
+    ;; Head and tail both start on [0,0]
+    [start-pos [[0 0] [0 0]]]
+    (->> input
+         str/split-lines
+         (map parse-line)
+         (mapcat cmd-to-moves)
+         (reductions next-segment-coords start-pos)
+         )))
+
+;; Record the tail position for the next move
+;; Accumulate the set of tail positions visited and then return the count
+;; of unique tail positions visited.
+(defn part1 [input]
+  (let [move-seq (process-input1 input)
+        tail-pos-set (reduce
+                       ;; Add the next tail position to the set of visited positions
+                       (fn [visited positions]
+                         ;(println "visited: " visited ", tail: " tail)
+                         (conj visited (last positions)))
+                       #{}
+                       move-seq)
+        ]
+    (count tail-pos-set)
+    ))
+
+;; Part 2
+
+(defn next-coords [start-positions move]
+  (let [head (first start-positions)
+        new-head (mapv + head move)
+        rest-pos (rest start-positions)]
+    (reductions next-tail new-head rest-pos)
+    ))
+(defn process-input2 [input]
+   (let
+     ;; Head and tail both start on [0,0]
+     [start-pos (vec (repeat 10 [0 0]))]
+     (->> input
+          str/split-lines
+          (map parse-line)
+          (mapcat cmd-to-moves)
+          (reductions next-coords start-pos)
+          )))
+
+(defn part2 [input]
+  (let [move-seq (process-input2 input)
+        tail-pos-set (reduce
+                       ;; Add the next tail position to the set of visited positions
+                       (fn [visited positions]
+                         ;(println "visited: " visited ", tail: " tail)
+                         (conj visited (last positions)))
+                       #{}
+                       move-seq)
+        ]
+    (count tail-pos-set)
+    ))
+
+
+;; TODO refactor: part1 and part2 are identical except for process-input function
+;; process-input1 and process-input2 are identical except start-pos and next-coords functions
